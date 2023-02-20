d4a9b0f7609cf15c47ec37a6718eb1445926bb63
victor odhiambo <emmanuelv428@gmail.com>
2023-02-19 21:35:15 -0800

    how to handle simple error in js

---
+++ sample.js
@@ -0,0 +1,9 @@
+let error1 = document.getElementById("error");
+
+
+function errorCode(){
+	console.log("something isn't right!")
+	//rendering error to the front page
+	error1.innerText = "something went wrong, please try again later";
+
+}
